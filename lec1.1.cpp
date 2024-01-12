f0d045ba1480d68a8b11e5ce8ce8cb7f3db3b183
WIN10 <hariyanijenish>
2024-01-12 17:33:17 +0530

    lec1c++

---
+++ lec1.1.cpp
@@ -0,0 +1,152 @@
+#include <iostream>
+using namespace std;
+
+    class Employees{
+    	public :
+    		int id;
+    		char name[100];
+    		char role[100];
+    		int age;
+    		int sailary;
+    		int experience;
+    		char city[100];
+    		char company[100];
+	};
+
+int main()
+{
+	Employees E1,E2,E3,E4,E5;
+	
+	cout << "Enter Employees's id";
+	cin >> E1.id;
+	cout << "Enter Employees's name";
+	cin >> E1.name;
+	cout << "Enter Employees's role";
+	cin >> E1.role;
+	cout << "Enter Employees's age";
+	cin >> E1.age;
+	cout << "Enter Employees's sailary";
+	cin >> E1.sailary;
+	cout << "Enter Employees's experience";
+	cin >> E1.experience;
+	cout << "Enter Employees's city";
+	cin >> E1.city;
+	cout << "Enter Employees's company";
+	cin >> E1.company;
+	
+	cout << " Employees's id\n" << E1.id ;
+	cout << " Employees's name\n" <<E1.name;
+	cout << " Employees's role\n" << E1.role;
+	cout << " Employees's age\n" << E1.age;
+	cout << " Employees's sailary\n" << E1.sailary;
+	cout << " Employees's experience\n" << E1.experience;
+	cout << " Employees's city\n" << E1.city;
+	cout << " Employees's company\n" << E1.company;
+	
+	cout << "Enter Employees's id";
+	cin >> E2.id;
+	cout << "Enter Employees's name";
+	cin >> E2.name;
+	cout << "Enter Employees's role";
+	cin >> E2.role;
+	cout << "Enter Employees's age";
+	cin >> E2.age;
+	cout << "Enter Employees's sailary";
+	cin >> E2.sailary;
+	cout << "Enter Employees's experience";
+	cin >> E2.experience;
+	cout << "Enter Employees's city";
+	cin >> E2.city;
+	cout << "Enter Employees's company";
+	cin >> E2.company;
+	
+	cout << " Employees's id\n" << E2.id ;
+	cout << " Employees's name\n" <<E2.name;
+	cout << " Employees's role\n" << E2.role;
+	cout << " Employees's age\n" << E2.age;
+	cout << " Employees's sailary\n" << E2.sailary;
+	cout << " Employees's experience\n" << E2.experience;
+	cout << " Employees's city\n" << E2.city;
+	cout << " Employees's company\n" << E2.company;
+	
+	cout << "Enter Employees's id";
+	cin >> E3.id;
+	cout << "Enter Employees's name";
+	cin >> E3.name;
+	cout << "Enter Employees's role";
+	cin >> E3.role;
+	cout << "Enter Employees's age";
+	cin >> E3.age;
+	cout << "Enter Employees's sailary";
+	cin >> E3.sailary;
+	cout << "Enter Employees's experience";
+	cin >> E3.experience;
+	cout << "Enter Employees's city";
+	cin >> E3.city;
+	cout << Enter Employees's company";
+	cin >> E1.company;
+	
+	cout << " Employees's id\n" << E3.id ;
+	cout << " Employees's name\n" <<E3.name;
+	cout << " Employees's role\n" << E3.role;
+	cout << " Employees's age\n" << E3.age;
+	cout << " Employees's sailary\n" << E3.sailary;
+	cout << " Employees's experience\n" << E3.experience;
+	cout << " Employees's city\n" << E3.city;
+	cout << " Employees's company\n" << E3.company;
+	
+	cout << "Enter Employees's id";
+	cin >> E4.id;
+	cout << "Enter Employees's name";
+	cin >> E4.name;
+	cout << "Enter Employees's role";
+	cin >> E4.role;
+	cout << "Enter Employees's age";
+	cin >> E4.age;
+	cout << "Enter Employees's sailary";
+	cin >> E4.sailary;
+	cout << "Enter Employees's experience";
+	cin >> E4.experience;
+	cout << "Enter Employees's city";
+	cin >> E4.city;
+	cout << "Enter Employees's company";
+	cin >> E4.company;
+	
+	cout << " Employees's id\n" << E4.id ;
+	cout << " Employees's name\n" <<E4.name;
+	cout << " Employees's role\n" << E4.role;
+	cout << " Employees's age\n" << E4.age;
+	cout << " Employees's sailary\n" << E4.sailary;
+	cout << " Employees's experience\n" << E4.experience;
+	cout << " Employees's city\n" << E4.city;
+	cout << " Employees's company\n" << E4.company;
+	
+	cout << "Enter Employees's id";
+	cin >> E5.id;
+	cout << "Enter Employees's name";
+	cin >> E5.name;
+	cout << "Enter Employees's role";
+	cin >> E5.role;
+	cout << "Enter Employees's age";
+	cin >> E5.age;
+	cout << "Enter Employees's sailary";
+	cin >> E5.sailary;
+	cout << "Enter Employees's experience";
+	cin >> E5.experience;
+	cout << "Enter Employees's city";
+	cin >> E5.city;
+	cout << "Enter Employees's company";
+	cin >> E5.company;
+	
+	cout << " Employees's id\n" << E5.id ;
+	cout << " Employees's name\n" <<E5.name;
+	cout << " Employees's role\n" << E5.role;
+	cout << " Employees's age\n" << E5.age;
+	cout << " Employees's sailary\n" << E5.sailary;
+	cout << " Employees's experience\n" << E5.experience;
+	cout << " Employees's city\n" << E5.city;
+	cout << " Employees's company\n" << E5.company;
+	return 0;
+}
+ 
+
---
+++ lec1.2.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+using namespace std;
+
+    class Employees{
+    	public :
+    		int id;
+    		char name[100];
+    		int color;
+    		int model;
+    		char company[100];
+	};
+
+int main()
+{
+	Employees c1,c2,c3,c4,c5;
+	
+	cout << "car's id";
+	cin >> c1.id;
+	cout << "car's name";
+	cin >> c1.name;
+	cout << "car's color";
+	cin >> c1.color;
+	cout << "car's experience";
+	cin >> c1.model;
+	cout << "car's company";
+	cin >> c1.company;
+	
+	cout << "car's id\n" << c1.id ;
+	cout << "car's name\n" <<c1.name;
+	cout << "car's color\n" << c1.color;
+	cout << "car's model\n" << c1.model;
+	cout << "car's company\n" << c1.company;
+	
+	cout << "car's id";
+	cin >> c2.id;
+	cout << "car's name";
+	cin >> c2.name;
+	cout << "car's color";
+	cin >> c2.color;
+	cout << "car's experience";
+	cin >> c2.model;
+	cout << "car's company";
+	cin >> c2.company;
+	
+	cout << "car's id\n" << c2.id ;
+	cout << "car's name\n" <<c2.name;
+	cout << "car's color\n" << c2.color;
+	cout << "car's model\n" << c2.model;
+	cout << "car's company\n" << c2.company;
+	cout << "Enter Employees's id";
+	
+	cout << "car's id";
+	cin >> c3.id;
+	cout << "car's name";
+	cin >> c3.name;
+	cout << "car's color";
+	cin >> c3.color;
+	cout << "car's experience";
+	cin >> c3.model;
+	cout << "car's company";
+	cin >> c3.company;
+	
+	cout << "car's id\n" << c3.id ;
+	cout << "car's name\n" <<c3.name;
+	cout << "car's color\n" << c3.color;
+	cout << "car's model\n" << c3.model;
+	cout << "car's company\n" << c3.company;
+	
+	cout << "car's id";
+	cin >> c4.id;
+	cout << "car's name";
+	cin >> c4.name;
+	cout << "car's color";
+	cin >> c4.color;
+	cout << "car's experience";
+	cin >> c4.model;
+	cout << "car's company";
+	cin >> c4.company;
+	
+	cout << "car's id\n" << c4.id ;
+	cout << "car's name\n" <<c4.name;
+	cout << "car's color\n" << c4.color;
+	cout << "car's model\n" << c4.model;
+	cout << "car's company\n" << c4.company;
+	return 0;
+}
+ 
+
